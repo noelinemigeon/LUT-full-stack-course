7632ed6075f7ced6d6c629c597fcc143d478a42e
noelinemigeon <>
2022-05-13 08:07:15 +0300

    Register Component, Validation & Flash Messages

--- mean-auth-app/anguar-src/src/app/app.module.ts
+++ mean-auth-app/anguar-src/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { FormsModule } from '@angular/forms';
+
+import { ValidateService } from './services/validate.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
   {path:'', component: HomeComponent},
@@ -30,9 +34,11 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- mean-auth-app/anguar-src/src/app/components/register/register.component.ts
+++ mean-auth-app/anguar-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from 'src/app/services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String; 
+  username:String;
+  email: String;
+  password:String;
 
-  constructor() { }
+  constructor(private ValidateService: ValidateService, private FlashMessages:FlashMessagesService) { }
 
   ngOnInit(): void {
   }
+
+  onRegisterSubmit(){
+    const user = {
+      name:this.name,
+      email:this.email,
+      username:this.username,
+      password:this.password
+    }
+
+    //Required Fields
+    if(!this.ValidateService.validateRegister(user)){
+      this.FlashMessages.show('Please fill in all fields',{cssClass: 'alert-danger',timeout: 3000});
+      return false;
+    }
+
+    //Validate Email
+    if(!this.ValidateService.validateEmail(user)){
+      this.FlashMessages.show('Please use a valid email',{cssClass: 'alert-danger',timeout: 3000});
+      return false;
+    }
+  } 
 
 }
