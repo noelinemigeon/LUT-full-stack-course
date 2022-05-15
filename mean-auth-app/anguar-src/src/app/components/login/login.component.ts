e651fbfbee18406090879a65f369453a0c11bcbc
noelinemigeon <>
2022-05-15 14:24:51 +0300

    Login & Logout

--- mean-auth-app/anguar-src/src/app/components/login/login.component.ts
+++ mean-auth-app/anguar-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router} from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
+  dataLogin: any;
 
-  constructor() { }
+  constructor(
+    private authService:AuthService,
+    private router:Router,
+    private flashMessage:FlashMessagesService
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  onLoginSubmit(){
+    const user = {
+      username: this.username,
+      password: this.password
+    } 
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      //console.log(data);
+      this.dataLogin=data;
+      if(this.dataLogin.success){
+        this.authService.storeUserData(this.dataLogin.token,this.dataLogin.user);
+        this.flashMessage.show('You are now logged in', {
+          cssClass: 'alert-success', 
+          timeout: 5000});
+        this.router.navigate(['dashboard']);
+      } else {
+        this.flashMessage.show(this.dataLogin.msg, {
+          cssClass: 'alert-danger', 
+          timeout: 5000});
+        this.router.navigate(['login']);
+      }
+    });
   }
 
 }
--- mean-auth-app/anguar-src/src/app/services/auth.service.ts
+++ mean-auth-app/anguar-src/src/app/services/auth.service.ts
@@ -19,4 +19,24 @@ export class AuthService {
     return this.http.post('http://localhost:3000/users/register', user, { headers });
   }
 
+  authenticateUser(user: any){
+    let headers = new HttpHeaders();
+    headers.append('Content-Type','application/json'); 
+    return this.http.post('http://localhost:3000/users/authenticate', user, { headers });
+  }
+
+  storeUserData(token:any, user:any){
+    localStorage.setItem('id_token',token);
+    localStorage.setItem('user',JSON.stringify(user));
+    this.authToken=token;
+    this.user=user;
+  };
+
+  logout(){
+    //set back the arguments to null
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear(); //clears local storage
+  }
+
 }
