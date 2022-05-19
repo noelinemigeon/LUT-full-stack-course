038741306efe7d1b614f655e6455a37ef0a985f2
noelinemigeon <>
2022-05-19 18:34:57 +0300

    heroku

--- mean-auth-app/config/database.js
+++ mean-auth-app/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     //database : 'mongodb://localhost:27017/meanauth',
-    //<database:'mongodb+srv://nono:<nono>@meanauthapp.w9sh3.mongodb.net/?retryWrites=true&w=majority',
-    database: 'mongosh "mongodb+srv://meanauthapp.w9sh3.mongodb.net/meanauthapp" --apiVersion 1 --username nono',
+    database:'mongodb+srv://nono:<nono>@meanauthapp.w9sh3.mongodb.net/?retryWrites=true&w=majority',
+    //database: 'mongosh "mongodb+srv://meanauthapp.w9sh3.mongodb.net/meanauthapp" --apiVersion 1 --username nono',
     secret : 'yoursecret'
 }
