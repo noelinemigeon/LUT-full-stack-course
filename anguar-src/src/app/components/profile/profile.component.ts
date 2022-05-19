257b40fbefa1f48d549603e37d5beee0b4dd018e
noelinemigeon <>
2022-05-19 16:18:11 +0300

    heroku

---
+++ anguar-src/src/app/components/profile/profile.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user:any ;
+  profileData:any;
+
+  constructor(
+    private authService:AuthService,
+    private router:Router
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.getProfile().subscribe(profile => {
+      this.profileData = profile;
+      this.user = this.profileData.user;
+    }, 
+    err => {
+      console.log(err);
+      return false;
+    });
+  }
+
+}
---
+++ anguar-src/src/app/services/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {map} from "rxjs/operators";
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authToken: any;
+  user: any;
+
+  constructor(private http:HttpClient) { }
+
+  registerUser(user: any){
+    let headers = new HttpHeaders();
+    headers.append('Content-Type','application/json');
+    //return this.http.post('http://localhost:3000/users/register', user, {headers: headers, observe: 'response'})
+    //.pipe(map((res: any) => res.json));
+    return this.http.post('users/register', user, { headers });
+  }
+
+  authenticateUser(user: any){
+    let headers = new HttpHeaders();
+    headers.append('Content-Type','application/json'); 
+    return this.http.post('users/authenticate', user, { headers });
+  }
+
+  getProfile(){
+    //let headers = new HttpHeaders();
+    this.loadToken(); //allows us to access the this.authToken
+    //headers.append('Authorization',this.authToken);
+    //headers.append('Content-Type','application/json'); 
+    let headers = new HttpHeaders({
+      'Content-Type':'application/json',
+      'Authorization': this.authToken
+    });
+
+    return this.http.get('users/profile', { headers });
+  }
+
+  storeUserData(token:any, user:any){
+    localStorage.setItem('id_token',token);
+    localStorage.setItem('user',JSON.stringify(user));
+    this.authToken=token;
+    this.user=user;
+  };
+
+  loadToken(){
+    //fetches token from localStorage
+    const token = localStorage.getItem('id_token')
+    this.authToken = token; 
+  }
+
+  loggedIn(){
+    this.loadToken();
+    const helper = new JwtHelperService();
+    return helper.isTokenExpired(this.authToken);
+  }
+
+  logout(){
+    //set back the arguments to null
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear(); //clears local storage
+  }
+
+}
