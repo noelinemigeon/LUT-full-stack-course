1a3ed7fae992da719aaa89dc22afa18b881e6dd3
noelinemigeon <>
2022-05-19 18:48:24 +0300

    Comments on Heroku deployment

--- mean-auth-app/config/database.js
+++ mean-auth-app/config/database.js
@@ -1,6 +1,5 @@
 module.exports = {
-    //database : 'mongodb://localhost:27017/meanauth',
-    database:'mongodb+srv://nono:nono@meanauthapp.w9sh3.mongodb.net/?retryWrites=true&w=majority',
-    //database: 'mongosh "mongodb+srv://meanauthapp.w9sh3.mongodb.net/meanauthapp" --apiVersion 1 --username nono',
+    //database : 'mongodb://localhost:27017/meanauth', //previous db (localhost)
+    database:'mongodb+srv://nono:nono@meanauthapp.w9sh3.mongodb.net/?retryWrites=true&w=majority', //linked db for heroku
     secret : 'yoursecret'
 }
